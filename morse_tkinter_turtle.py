13f72de527a18ebda880e6d99b52105bc86befb4
Omkar Pradhan <42708220+omkarpradhan12@users.noreply.github.com>
2023-03-10 12:52:46 +0530

    updated morse tkinter turtle

--- morse_tkinter_turtle.py
+++ morse_tkinter_turtle.py
@@ -3,7 +3,7 @@ import tkinter
 
 root = tkinter.Tk()
 root.geometry('500x500-5+40') #added by me
-cv = tu.ScrolledCanvas(root, width=900, height=900)
+cv = tu.ScrolledCanvas(root, width=1500000, height=1500000)
 cv.pack()
 
 letters = list("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split())
@@ -16,20 +16,28 @@ letters_to_morse
 bind = {'.':2,"-":30}
 
 screen = tu.TurtleScreen(cv)
-screen.screensize(2000,1500) #added by me
+screen.screensize(1500000,1500000) #added by me
 t = tu.RawTurtle(screen)
 
 
 t.pensize(10)
 t.ht()
 
-for i in "GITHUB":
-    for j in letters_to_morse[i]:
+for i in "OMKAR is panda".upper():
+    if i !=" ":
+        for j in letters_to_morse[i]:
+            t.pd()
+            t.fd(bind[j])
+            t.pu()
+            t.fd(20)
+            t.pd()
+        print(i)
+    if i==" ":
         t.pd()
-        t.fd(bind[j])
-        t.pu()
+        t.lt(90)
         t.fd(20)
-        t.pd()
+        t.bk(20)
+        t.rt(90)
     t.pu()
     t.fd(50)
 root.mainloop()
